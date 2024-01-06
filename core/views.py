756dc551ab7edcd48aee17617b030462861f9b2d
Timothy Miamba <timal0361@gmail.com>
2024-01-06 13:26:02 +0200

    Add signup page

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from item.models import Category, Item
 
+from .forms import SignupForm
+
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
@@ -12,3 +14,9 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
